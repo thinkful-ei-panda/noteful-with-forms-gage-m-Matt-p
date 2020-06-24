1447ef6c5853aae9f1c908da54e3d79b3e7abef1
Matt <63121708+themlp101@users.noreply.github.com>
2020-06-24 12:46:08 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import Main from './components/Main'
+import AddNote from './components/AddNote'
+import FolderList from './components/FolderList'
+import AddNav from './components/AddNav'
+import AddFolderMain from './components/AddFolderMain'
+import NoteMain from './components/NoteMain'
+import NoteNav from './components/NoteNav'
+import store from './store'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+class App extends Component {
+	state = {
+		store: { ...store },
+	}
+
+	render() {
+		return (
+			<div className='App'>
+				<Switch>
+					<Route
+						path='/folder/:folderid'
+						render={(routeProps) => (
+							<Sidebar
+								notes={this.state.store.notes}
+								folders={this.state.store.folders}
+							/>
+						)}
+					/>
+					<Route
+						path='/note/:noteid'
+						render={(routeProps) => (
+							<NoteNav
+								folders={this.state.store.folders}
+								notes={this.state.store.notes}
+								{...routeProps}
+							/>
+						)}
+					/>
+					<Route
+						path='/add-folder'
+						render={(routeProps) => <AddNav {...routeProps} />}
+					/>
+
+					<Route
+						path='/add-note'
+						render={(routeProps) => <AddNav {...routeProps} />}
+					/>
+					<Route
+						path='/'
+						exact
+						render={(routeProps) => (
+							<Sidebar
+								notes={this.state.store.notes}
+								folders={this.state.store.folders}
+							/>
+						)}
+					/>
+				</Switch>
+
+				<Header />
+
+				<Switch>
+					<Route
+						path='/folder/:folderid'
+						render={(props) => (
+							<FolderList notes={this.state.store.notes} {...props} />
+						)}
+					/>
+					<Route
+						path='/note/:noteid'
+						render={(routeProps) => (
+							<NoteMain
+								notes={this.state.store.notes}
+								{...routeProps}
+							/>
+						)}
+					/>
+					<Route
+						path='/add-folder'
+						render={(props) => (
+							<AddFolderMain
+								notes={this.state.store.notes}
+								folders={this.state.store.folders}
+							/>
+						)}
+					/>
+					<Route
+						path='/add-note'
+						render={() => (
+							<AddNote
+								notes={this.state.store.notes}
+								folders={this.state.store.folders}
+							/>
+						)}
+					/>
+					<Route
+						path='/'
+						exact
+						render={() => <Main notes={this.state.store.notes} />}
+					/>
+				</Switch>
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
